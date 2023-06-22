cea17ba912d4911b791edc6d5c67a13890aa965b
Gildedboy <knukles30@gmail.com>
2023-06-22 10:54:15 -0700

    find elements with xpath and css selectors and use dollar sign for using them

--- test/specs/home.js
+++ test/specs/home.js
@@ -15,6 +15,29 @@ describe("Home", () => {
     await browser.url("https://practice.automationbro.com/about/");
 
     // Assert URL
-    await expect(browser).toHaveUrl("https://practice.automationbro.com/about/ ");
+    await expect(browser).toHaveUrl("https://practice.automationbro.com/about/");
+  });
+
+  it("Click get started btn & assert url contains get-started text ", async () => {
+    //Open Home Page
+    await browser.url("https://practice.automationbro.com/");
+
+    //Click get started button
+    await $(`#get-started`).click();
+
+    //assert url contains get-started text
+    await expect(browser).toHaveUrlContaining("get-started");
+  });
+
+  it("Click Logo Button & assert URL does not contains #get-started text", async () => {
+    //Open Home Page
+    await browser.url("https://practice.automationbro.com/");
+
+    //Click logo button
+
+    await $(`//img[@alt="Practice E-Commerce Site"]`).click();
+
+    //Check url does not contains get-started
+    await expect(browser).not.toHaveUrlContaining("get-started");
   });
 });
