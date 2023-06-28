b4ed51f750f8c65555ce0127eb0e90cf9a33c65b
Gildedboy <knukles30@gmail.com>
2023-06-28 12:06:03 -0700

    Only test contact for the moment

--- wdio.conf.js
+++ wdio.conf.js
@@ -24,7 +24,7 @@ exports.config = {
   //
   specs: [
     // ToDo: define location for spec files here
-    "./test/specs/**/*.js",
+    "./test/specs/**/contact.js",
   ],
   // Patterns to exclude.
   exclude: [
